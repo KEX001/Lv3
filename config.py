d1d47807dcdbac36896b470e2318056dda829318
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2025-01-16 13:03:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/kxunal/Lv3",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "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"
 )  # Fill this variable if your upstream repository is private
